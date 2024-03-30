c89867d01109f62f862b820acb33fed62f0526ef
Anes Dzehverovic <anesdzehverovic@gmail.com>
2024-03-30 13:46:56 +0100

    added html instead of plain text

--- email_sender.py
+++ email_sender.py
@@ -1,21 +1,22 @@
+import pandas as pd
 import smtplib
 import ssl
 from email.mime.multipart import MIMEMultipart
-from email.mime.application import MIMEApplication
 from email.mime.text import MIMEText
+from email.mime.application import MIMEApplication
 from email.mime.image import MIMEImage
-import pandas as pd
-
+from string import Template
 
 def email_sender(dataset: pd.DataFrame,
-         name: str,
-         surname: str,
-         email_sender: str,
-         email_password: str,
-         subject_text: str,
-         email_text: str, 
-         sales_deck = None,
-         signature = None):
+                 name: str,
+                 surname: str,
+                 email_sender: str,
+                 email_password: str,
+                 subject_text: str,
+                 email_text: str, 
+                 email_html_path: str, 
+                 sales_deck=None,
+                 signature_image_path=None):
     """
     Send personalized emails to recipients based on the data provided.
 
@@ -54,50 +55,56 @@ def email_sender(dataset: pd.DataFrame,
         
         `Keep this password confidential and secure.`
     """
-    df = pd.read_csv(dataset) 
-    data = df[["First Name","Email"]]
+    
+    df = pd.read_csv(dataset)
+    data = df[["First Name", "Email"]]
+
+    with open(email_html_path, 'r', encoding="utf-8") as file:
+        email_html_template = file.read()
+    
+    template = Template(email_html_template)
 
     for index, row in data.iterrows():
         client_name = row["First Name"]
         email_receiver = row["Email"] 
 
         em = MIMEMultipart()
-
-        # Open the subject.txt file for reading
-        with open(f"emails/{subject_text}", 'r', encoding="utf-8") as file:
-            subject_open= file.read()
         
-        subject_content = f"{client_name}, {subject_open}"
+        with open(subject_text, 'r', encoding="utf-8") as file:
+            subject_content = file.read().format(name=client_name)
 
-        # Open the subject.txt file for reading
-        with open(f"emails/{email_text}", 'r', encoding="utf-8") as file:
-            email_open = file.read()
+        with open(email_text, 'r', encoding="utf-8") as file:
+            email_body_content = file.read()
 
-        email_content = f"Hi {client_name}, {email_open}"
-
+        email_body_content = email_body_content.replace("\n", "<br>")
+        email_content = template.substitute(name=client_name, text=email_body_content)
+       
+        if signature_image_path:
+            signature_html = f'<img src="cid:{signature_image_path}" alt="signature">'
+            email_content += signature_html  
+        
         em['From'] = f"{name} {surname}"
         em['To'] = email_receiver
         em['Subject'] = subject_content
         
-        email_body = MIMEText(email_content, 'plain')
+        email_body = MIMEText(email_content, 'html')
         em.attach(email_body)
         
-        if sales_deck is not None:
-            with open(f"attachments/{sales_deck}", 'rb') as pdf_file:
-                    slide_deck = MIMEApplication(pdf_file.read(), _subtype='pdf')
-                    slide_deck.add_header('content-disposition', 'attachment', filename=f"{sales_deck}")
-                    em.attach(slide_deck)
-                #slide deck should be attached on the first email only.
-        if signature is not None:
-            with open(f"attachments/{signature}", 'rb') as image_file: #signature is attached here
+        if sales_deck:
+            with open(sales_deck, 'rb') as pdf_file:
+                slide_deck = MIMEApplication(pdf_file.read(), _subtype='pdf')
+                slide_deck.add_header('content-disposition', 'attachment', filename=sales_deck)
+                em.attach(slide_deck)
+
+        if signature_image_path:
+            with open(signature_image_path, 'rb') as image_file:
                 image = MIMEImage(image_file.read())
-                image.add_header('Content-Disposition', 'attachment', filename=f"{signature}")
+                image.add_header('Content-ID', f'<{signature_image_path}>')  
                 em.attach(image)
         
         context = ssl.create_default_context()
-
-        with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) as smtp: #sending the email
-            smtp.login(email_sender,email_password)
+        with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) as smtp:
+            smtp.login(email_sender, email_password)
             smtp.sendmail(email_sender, email_receiver, em.as_string())
         
         print(f"Email sent to {client_name} at {email_receiver}")
--- main.py
+++ main.py
@@ -10,10 +10,11 @@ if __name__ == "__main__":
           email_password = info.generated_app_password, 
           #everything bellow this comment line can be changed to your own custom files.
           dataset = "example_dataset.csv",
-          subject_text="subject.txt", 
-          email_text="email_1.txt",
-          sales_deck="sample.pdf",
-          signature="image.png"
+          subject_text="emails/subject.txt", 
+          email_html_path="emails/html_email_1.html",
+          email_text="emails/email_1.txt",
+          sales_deck=None,
+          signature_image_path="attachments/sample_image.png"
           )
 
 
